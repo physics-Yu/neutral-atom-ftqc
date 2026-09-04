2d7f551e8160a69fee5ac67fe28ad26ab2066d3d
physics-Yu <yuyq24@mails.tsinghua.edu.cn>
2026-09-04 17:01:18 +0800

    Scaffold logical IR module

---
+++ src/compiler/logical_ir.py
@@ -0,0 +1,5 @@
+"""Logical/QEC intermediate representation.
+
+This layer may contain logical initialization, logical Clifford operations, and QEC macros.
+It is not executable by the physical scheduler.
+"""
